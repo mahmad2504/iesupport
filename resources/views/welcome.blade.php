ac9cf69c925303a829957cd493492e840abefd54
mahmad2504 <ahmad-mumtaz1@hotmail.com>
2020-07-16 20:45:07 +0500

    M

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/dygraph/2.1.0/dygraph.min.css" />
+		<link rel="stylesheet" href="css/dygraph.css" />
         <!-- Styles -->
         <style>
 		
